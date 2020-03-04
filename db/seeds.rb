2f8264e7acf2b087bb9833bfdc9a3174f6e3d62e
Tony Ye <tonyyye@gmail.com>
2020-03-04 11:05:00 -0800

    Change ordering of application.js files and change application.css to scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u1 = User.create(username: 'Sionar', email: 'tonyyye@gmail.com', password: 'password')
+u2 = User.create(username: 'Guest', email: 'guest@dischordapp.com', password: 'password')
