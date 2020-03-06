866834b03c884c0cbac82b036f689edaf85f95de
Tony Ye <tonyyye@gmail.com>
2020-03-06 11:14:34 -0800

    Add styling to main component

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 class Main extends React.Component {
   constructor(props) {
@@ -7,7 +8,23 @@ class Main extends React.Component {
 
   render() {
     return (
-      <div>Hi!</div>
+      <div id="main-container">
+        <section id="server-bar"></section>
+        <div id="channel-bar-container">
+          <section id="server-name-button"></section>
+          <section id="channel-bar"></section>
+          <section id="user-name-button"></section>
+        </div>
+        
+        <div id="content-outer-container">
+            <section id="header-bar"></section>
+          <div id="content-body-container">
+            <section id="messages-box"></section>
+            <section id="users-bar"></section>
+          </div>
+
+        </div>
+      </div>
     )
   }
 }
