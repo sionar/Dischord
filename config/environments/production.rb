a2d0e89e29b89f048a754254d42088778da7b07d
Tony Ye <tonyyye@gmail.com>
2020-03-12 17:33:31 -0700

    add redis config lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_socket_server_url = "wss://dischord-app.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://dischord-app.herokuapp.com', 'http://dischord-app.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create, :update]
